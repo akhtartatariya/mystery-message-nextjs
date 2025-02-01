60755f0c83a866dd1159bdc28e8cfe234392540c
Akhtar <137376031+akhtartatariya@users.noreply.github.com>
2025-02-01 18:07:34 +0530

    Refactor sign-in page to use next/router for navigation and clean up code formatting

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -4,7 +4,7 @@
 import { useState } from "react"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
-import  { AxiosError } from "axios"
+import { AxiosError } from "axios"
 import { z } from "zod"
 import ApiResponse from "@/types/ApiResponse"
 import { useToast } from "@/hooks/use-toast"
@@ -12,16 +12,17 @@ import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { Loader2 } from "lucide-react"
-import { useRouter } from "next/navigation"
 import Link from "next/link"
 import { signInSchema } from "@/zodSchemas/signInSchema"
 import { signIn } from "next-auth/react"
+import { useRouter } from "next/router"
 
 const SignUp = () => {
   const [isFormSubmit, setIsFormSubmit] = useState(false)
   const { toast } = useToast()
 
   const router = useRouter()
+  
   const form = useForm<z.infer<typeof signInSchema>>({
     resolver: zodResolver(signInSchema),
     defaultValues: {
@@ -45,10 +46,9 @@ const SignUp = () => {
           description: response.error
         })
       }
-      console.log("response",response)
-      if(response?.url){
-        // router.push('/dashboard')
-        router.replace('/dashboard')
+      console.log("response", response)
+      if (response?.url) {
+        router.push('/dashboard')
       }
     } catch (err) {
       const axiosError = err as AxiosError<ApiResponse>
@@ -61,48 +61,48 @@ const SignUp = () => {
     }
   }
 
-return (
-  <div className="flex justify-center items-center min-h-screen bg-gray-800">
-    <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
-      <div className="text-center">
-        <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
-          Join True Feedback
-        </h1>
-        <p className="mb-4">Sign in to start your anonymous adventure</p>
-      </div>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          
-          <FormField
-            control={form.control}
-            name="identifier"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email/Username</FormLabel>
-                <FormControl>
-                  <Input placeholder="Email or Username" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Password</FormLabel>
-                <FormControl>
-                  <Input type="password" placeholder=" ••••••••" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit" disabled={isFormSubmit}>{isFormSubmit ? <Loader2 className="w-5 h-5 animate-spin" /> : "Sign in"}</Button>
-        </form>
-      </Form>
-      <div className="text-center mt-4">
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-800">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Join True Feedback
+          </h1>
+          <p className="mb-4">Sign in to start your anonymous adventure</p>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+
+            <FormField
+              control={form.control}
+              name="identifier"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email/Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Email or Username" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder=" ••••••••" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" disabled={isFormSubmit}>{isFormSubmit ? <Loader2 className="w-5 h-5 animate-spin" /> : "Sign in"}</Button>
+          </form>
+        </Form>
+        <div className="text-center mt-4">
           <p>
             Not a member yet?{' '}
             <Link href="/sign-up" className="text-blue-600 hover:text-blue-800">
@@ -110,9 +110,9 @@ return (
             </Link>
           </p>
         </div>
+      </div>
     </div>
-  </div>
-)
+  )
 
 }
 export default SignUp
