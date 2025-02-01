1a08df99058b4517381fb2d209ca940acbc9d5b5
Akhtar <137376031+akhtartatariya@users.noreply.github.com>
2025-02-01 19:41:23 +0530

    Refactor sign-in page to enable authenticated redirection to dashboard

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -61,11 +61,11 @@ const SignUp = () => {
       setIsFormSubmit(false)
     }
   }
-  // useEffect(() => {
-  //   if (status === "authenticated") {
-  //     router.push('/dashboard')
-  //   }
-  // }, [status, router, session])
+  useEffect(() => {
+    if (status === "authenticated") {
+      router.push('/dashboard')
+    }
+  }, [status, router, session])
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-800">
       <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
